5facf981ad13db7b4871e6d07b33a5d67928d885
nahm137 <nubaidahmed2006@gmail.com>
2026-07-26 11:45:06 -0400

    Add handling for 'Suspicious Keyword' in scoring

--- src/RiskScorer.java
+++ src/RiskScorer.java
@@ -4,8 +4,10 @@ import model.AnalysisResult;
 import java.util.ArrayList;
 
 public class RiskScorer {
+    
     public AnalysisResult score (ArrayList<String> flags){
         int tempScore = 0;
+        
         for(String flag : flags) {
             if (flag.contains("Possible typosquatting:")){
                 tempScore+= 10;
@@ -19,12 +21,11 @@ public class RiskScorer {
             else if(flag.contains("Suspicious URL (contains numbers):")){
                 tempScore += 3;
             }
-            /* commented out because i dont know the name of the flags yet.
-            else if(flag.contains("Suspicious Keyword:") {
-                tempScore += 3; //tentative
+            else if (flag.contains("Suspicious Keyword:")) {
+                tempScore += 3;
             }
-             */
         }
+        
         return new AnalysisResult(tempScore);
     }
 }
